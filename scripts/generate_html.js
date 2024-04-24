9bd7b2077ab3e5c9031878c1b845270d9ddbf381
Luke-Fernando <isanjaya139@gmail.com>
2024-04-24 23:26:22 +0530

    change the layout and name elements with readable classes

--- scripts/generate_html.js
+++ scripts/generate_html.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 function generateListHTML(json) {
   const definition = JSON.parse(fs.readFileSync(`sheet_definitions/${json}`));
-  const variants = definition.variants
-  const name = definition.name
-  const typeName = definition.type_name
+  const variants = definition.variants;
+  const name = definition.name;
+  const typeName = definition.type_name;
 
   var requiredSexes = [];
   var previewRow = 10;
   if (definition.preview_row !== undefined) {
-    previewRow = definition.preview_row
+    previewRow = definition.preview_row;
   }
   if (definition.layer_1.male !== undefined) {
     requiredSexes.push("male");
@@ -33,16 +33,20 @@ function generateListHTML(json) {
 
   const requiredSex = requiredSexes.join(",");
 
-  const startHTML = `<li data-required="[REQUIRED_SEX]"><span class="condensed">${name}</span><ul>`.replace("[REQUIRED_SEX]", requiredSex);
-  const templateHTML = fs.readFileSync("scripts/template-general.html", 'utf8');
+  const startHTML =
+    `<li data-required="[REQUIRED_SEX]" class="ucg-customizer-chooser-list-item"><span class="condensed ucg-customizer-chooser-list-title">${name}</span><ul class="ucg-customizer-chooser-list">`.replace(
+      "[REQUIRED_SEX]",
+      requiredSex
+    );
+  const templateHTML = fs.readFileSync("scripts/template-general.html", "utf8");
 
-  const endHTML = '</ul></li>';
+  const endHTML = "</ul></li>";
 
   var idx = 0;
-  var listItemsHTML = `<li><input type="radio" id="${typeName}-none" name="${typeName}"> <label for="${typeName}-none">No ${typeName}</label></li>`;
+  var listItemsHTML = `<li class="ucg-customizer-chooser-list-item"><input type="radio" id="${typeName}-none" name="${typeName}"> <label for="${typeName}-none">No ${typeName}</label></li>`;
   for (variant in variants) {
     const itemName = variants[idx];
-    const itemIdFor = typeName + "-" + name.replaceAll(" ", "_") +  "_" + itemName.replaceAll(" ", "_");
+    const itemIdFor = typeName + "-" + name.replaceAll(" ", "_") + "_" + itemName.replaceAll(" ", "_");
     var matchBodyColor = false;
     if (definition[`match_body_color`] !== undefined) {
       matchBodyColor = true;
@@ -50,7 +54,7 @@ function generateListHTML(json) {
     var dataFiles = "";
     var sexIdx = 0;
     for (sex in requiredSexes) {
-      for (jdx =1; jdx < 10; jdx++) {
+      for (jdx = 1; jdx < 10; jdx++) {
         const layerDefinition = definition[`layer_${jdx}`];
         if (layerDefinition !== undefined) {
           if (sexIdx === 0) {
@@ -58,12 +62,12 @@ function generateListHTML(json) {
             dataFiles += "data-preview_row=" + previewRow + " data-layer_" + jdx + "_zpos=" + zPos + " ";
             const custom_animation = layerDefinition.custom_animation;
             if (custom_animation !== undefined) {
-              dataFiles += `data-layer_${jdx}_custom_animation=` + custom_animation + " "
+              dataFiles += `data-layer_${jdx}_custom_animation=` + custom_animation + " ";
             }
           }
-          const file = layerDefinition[requiredSexes[sexIdx]]
+          const file = layerDefinition[requiredSexes[sexIdx]];
           if (file !== null && file !== "") {
-            dataFiles += "data-layer_" + jdx + "_" + requiredSexes[sexIdx] + "=\"" + file + itemName.replaceAll(" ", "_") + ".png\" ";
+            dataFiles += "data-layer_" + jdx + "_" + requiredSexes[sexIdx] + '="' + file + itemName.replaceAll(" ", "_") + '.png" ';
           }
         } else {
           break;
@@ -84,27 +88,27 @@ function generateListHTML(json) {
   return startHTML + listItemsHTML + endHTML;
 }
 
-var lineReader = require('readline').createInterface({
-  input: fs.createReadStream('source_index.html')
+var lineReader = require("readline").createInterface({
+  input: fs.createReadStream("source_index.html"),
 });
-var htmlGenerated = '<!-- THIS FILE IS AUTO-GENERATED. PLEASE DONT ALTER IT MANUALLY -->\n';
+var htmlGenerated = "<!-- THIS FILE IS AUTO-GENERATED. PLEASE DONT ALTER IT MANUALLY -->\n";
 
-lineReader.on('line', function (line) {
-  if (line.includes('div_sheet_')) {
-    const definition = line.replace("div_sheet_","");
-    const newLine = generateListHTML(`${definition}.json`.replaceAll("\t", ""))
-    htmlGenerated+=newLine+"\n";
+lineReader.on("line", function (line) {
+  if (line.includes("div_sheet_")) {
+    const definition = line.replace("div_sheet_", "");
+    const newLine = generateListHTML(`${definition}.json`.replaceAll("\t", ""));
+    htmlGenerated += newLine + "\n";
   } else {
-    htmlGenerated+=line+"\n";
+    htmlGenerated += line + "\n";
   }
 });
 
-lineReader.on('close', function (line) {
-  fs.writeFile('index.html', htmlGenerated, function(err) {
-            if (err) {
-                return console.log(err);
-            } else {
-                console.log('Updated!');
-            }
-    });
+lineReader.on("close", function (line) {
+  fs.writeFile("index.html", htmlGenerated, function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("Updated!");
+    }
+  });
 });
